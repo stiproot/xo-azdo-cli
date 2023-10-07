aadf35a9ca288e55e4adcd9fb56e39c1c66dd3f0
stiproot <stipcich.simon@gmail.com>
2023-10-07 12:45:14 +0200

    chore:minor cleanup.

--- src/Xo.AzDO.Engine/Providers/CloneWiCmdProvider.cs
+++ src/Xo.AzDO.Engine/Providers/CloneWiCmdProvider.cs
@@ -4,12 +4,5 @@ public class CloneWiCmdProvider : IProvider<CloneWiCmd>
 {
     public CloneWiCmd Provide() => new CloneWiCmd
     {
-        Id = 1116048,
-        ParentId = 1101238,
-        IterationName = "Sprint 13 2023",
-        IterationBasePath = "Software\\Non-Aligned\\Customers and Emerging Markets\\Teams\\N2 Chapmans Peak Project Team\\2023",
-        TeamName = "CEM - N2 Chapmans Peak Project Team",
-        AreaPath = "Software\\Customers and Emerging Markets\\Rapid Response\\Project\\N2 Chapmans Peak Project Team",
-        Tags = "sibot-sat-001",
     };
 }
--- src/Xo.AzDO.Engine/Providers/DashboardWorkflowCmdProvider.cs
+++ src/Xo.AzDO.Engine/Providers/DashboardWorkflowCmdProvider.cs
@@ -4,34 +4,5 @@ public class DashboardWorkflowCmdProvider : IProvider<CreateDashboardWorkflowCmd
 {
     public CreateDashboardWorkflowCmd Provide() => new CreateDashboardWorkflowCmd
     {
-        IterationName = "Sprint 14 2023",
-        IterationBasePath = "Software\\Non-Aligned\\Customers and Emerging Markets\\Teams\\N2 Chapmans Peak Project Team\\2023",
-        TeamName = "CEM - N2 Chapmans Peak Project Team",
-        DashboardName = "Chapmans Peak Sprint 14 2023",
-        QueryFolderBasePath = "Shared Queries/Customers and Emerging Markets/Rapid Response/N2 Chapmans Peak Project Team",
-        Initiatives = new List<Initiative>
-        {
-            new()
-            {
-                Title = "HDT - Product Enhancements",
-                Tag = "N2CP-IS-HelpDesk",
-                Desc= "",
-                QueryFolderName = "Sprint 14 2023"
-            },
-            new()
-            {
-                Title = "Reboot",
-                Tag = "N2CP-Reboot",
-                Desc = "This goal is to spend 30% of our time towards process improvement, fixing technical debt and approved learning.",
-                QueryFolderName = "Sprint 14 2023"
-            },
-            new()
-            {
-                Title = "Dreadnought Training",
-                Tag = "N2CP-DreadnoughtTraining",
-                Desc = "",
-                QueryFolderName = "Sprint 14 2023"
-            }
-        },
     };
 }
