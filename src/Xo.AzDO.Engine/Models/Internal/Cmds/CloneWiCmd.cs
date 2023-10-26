a90598459935dac447b183b6e3f69fb1381a40f5
stiproot <stipcich.simon@gmail.com>
2023-10-26 08:15:03 +0200

    chore:remove unused team-name model prop.

--- src/Xo.AzDO.Engine/Models/Internal/Cmds/CloneWiCmd.cs
+++ src/Xo.AzDO.Engine/Models/Internal/Cmds/CloneWiCmd.cs
@@ -5,7 +5,6 @@ public class CloneWiCmd : IProcessorCmd
 	public int Id { get; init; } = 0;
 	public int ParentId { get; init; } = 0;
 	public string IterationPath { get; init; } = string.Empty;
-	public string TeamName { get; init; } = string.Empty;
 	public string AreaPath { get; init; } = string.Empty;
 	public string Tags { get; init; } = string.Empty;
 }
