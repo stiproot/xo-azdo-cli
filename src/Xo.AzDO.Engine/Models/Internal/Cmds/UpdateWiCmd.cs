09765079837a1a0fc5de58cdc74d213611622ab4
stiproot <stipcich.simon@gmail.com>
2024-01-24 15:13:47 +0200

    fix:nullable complete property in update cmd.

--- src/Xo.AzDO.Engine/Models/Internal/Cmds/UpdateWiCmd.cs
+++ src/Xo.AzDO.Engine/Models/Internal/Cmds/UpdateWiCmd.cs
@@ -3,5 +3,5 @@ namespace Xo.AzDO.Engine.Models;
 public class UpdateWiCmd : BaseWiCmd, IProcessorCmd
 {
     public int id { get; init; }
-    public int complete { get; init; }
+    public int? complete { get; init; }
 }
